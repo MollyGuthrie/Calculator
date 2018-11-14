460747a0490dce67f0acedb59a756bee6b9b44e2
Molly Guthrie <Molly.Guthrie@bissell.com>
2018-11-14 12:28:05 -0500

    Added Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+"""
+Date: November 14, 2018
+
+References: Youtube Git Tutorial for Beginners
+
+What I want to accomplish in this commit:
+- Welcome user to the Calculator
+- Get project on Github
+"""
+
+print("Welcome to Molly's Calculator!")
