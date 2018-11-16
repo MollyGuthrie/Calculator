cc27d66f5827e787e500d6844611162adb5137cd
Molly Guthrie <mollyguthrie@Mollys-MacBook-Air.local>
2018-11-16 12:26:26 -0500

    adding if/elif/else for numbers allowed

--- Calculator.py
+++ Calculator.py
@@ -1,12 +1,27 @@
-#Date: November 15, 2018
+#Date: November 16, 2018
 
-print("Calculator Program")
-
-userName = input("What is your name: ")
+userName = input("Welcome to the Calculator. \nWhat is your name: ")
 print("Welcome " + userName.strip().title() + "!")
+print("\nWe only accept numbers between 0 and 100")
+
+tooLow = ("Your number is too low!")
+tooHigh = ("Your number is too high!")
 
 firstNumber= float(input("Please enter your first number: "))
+if firstNumber <0:
+    print(tooLow)
+elif firstNumber >100:
+    print(tooHigh)
+else:
+    print("good selection!")
+
 secondNumber= float(input("Please enter your second number: "))
+if secondNumber <0:
+    print(tooLow)
+elif secondNumber >100:
+    print(tooHigh)
+else:
+    print("good selection")
 
 print('{} + {} = '.format(firstNumber, secondNumber), end="")
 print(firstNumber+secondNumber)
@@ -19,3 +34,5 @@ print(firstNumber*secondNumber)
 
 print('{} / {} = '.format(firstNumber, secondNumber), end="")
 print(firstNumber/secondNumber)
+
+print("\nThank you!")
